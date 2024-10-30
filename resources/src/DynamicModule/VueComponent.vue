0c3364e9ea1b047482ecf2b1b269e23bcdd059b9
Александр Болотов <asbolotov@inno.tech>
2024-10-30 15:32:08 +0300

    fix reactive props

--- resources/src/DynamicModule/VueComponent.vue
+++ resources/src/DynamicModule/VueComponent.vue
@@ -1,42 +1,59 @@
 <script>
-const { useSlots, h, defineAsyncComponent, Suspense } = require('vue');
+const {
+  useSlots,
+  h,
+  defineAsyncComponent,
+  Suspense,
+  toRef,
+  unref,
+} = require("vue");
 
 export default {
   name: "ModuleComponent",
   props: {
     name: {
       type: String,
-      required: true
+      required: true,
     },
     timeout: [String, Number],
-    suspensible: Boolean
+    suspensible: Boolean,
   },
-  emits: ['fail'],
+  emits: ["fail"],
   setup(props, ctx) {
     const slots = useSlots();
 
     const Component = defineAsyncComponent(() => {
       return new Promise((resolve, reject) => {
-          mw.loader.using([props.name], function ( require ) {
+        mw.loader
+          .using([props.name], function (require) {
             resolve(require(props.name));
-          } ).catch((err) => {
-            ctx.emit('fail')
+          })
+          .catch((err) => {
+            ctx.emit("fail");
             reject(err);
           });
-      })
+      });
     });
 
-    const suspenseSlots = { default: h(Component, ctx.attrs, slots) }
+    const attrRef = toRef(ctx, "attrs");
+    const suspenseSlots = { default: h(Component, attrRef, slots) };
 
     if (slots.fallback) {
       suspenseSlots.fallback = slots.fallback();
     }
 
-    return () => h(
+    return () =>
+      h(
         Suspense,
         { timeout: props.timeout, suspensible: props.suspensible },
-        suspenseSlots
-    );
-  }
+        {
+          ...suspenseSlots,
+          default: {
+            ...suspenseSlots.default,
+            props: unref(suspenseSlots.default.props),
+          },
+        }
+      );
+  },
 };
 </script>
